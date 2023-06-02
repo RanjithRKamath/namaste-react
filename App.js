0ebe795c0b242f4641fd6a34c7f43befb624fca5
unknown <ranjithrkamath@gmail.com>
2023-06-02 06:00:49 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,17 @@
+const result = React.createElement(
+  "div",
+  { id: "parent" },
+  [React.createElement(
+    "div",
+    { id: "child1", key:"child1" },
+    [React.createElement("h1", {key:"h1"}, "this is h1 tag"),
+    React.createElement("h2", {key:"h2"}, "this is h2 tag")]
+  ), React.createElement(
+    "div",
+    { id: "child2", key:"child2" },
+    [React.createElement("h3", {key:"h31"}, "this is h3 tag"),
+    React.createElement("h4", {key:"h4"}, "this is h4 tag")]
+  )]
+);
+const root1 = ReactDOM.createRoot(document.getElementById("root"));
+root1.render(result);
